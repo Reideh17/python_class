cbe2202e2a73a2270ff3d3dd9260f809711ea832
Reideh17 <hedier.alvarez@gmail.com>
2021-06-22 22:45:22 -0500

    reto 5

--- reto4.py
+++ reto4.py
@@ -1,14 +1,29 @@
 n, k= input().split()
 n = int(n)
 k = int(k)
-if n not in range(1, 1001):
-  print('fuera de rango')
-  if k<1 and k>1000:
-    print('fuera de rango')
-else:
-  valores = input().split() 
-  valores_int = []
-  for i in valores:
-    valores_int.append(int(i))
-  print(valores)
-  print(valores_int)
+
+laminas = input().split()
+
+if n not in range(1, 10001):    
+    n = 1
+
+if k not in range(1, 10001):    
+    k = 1
+
+mi_diccionario = {}
+laminas_rep = 0
+laminas_en_memoria = 0
+
+for posicion, value  in enumerate(laminas):
+  if value in mi_diccionario:
+    laminas_rep += 1
+    if posicion - mi_diccionario[value] <= k:
+      laminas_en_memoria += 1
+  mi_diccionario[value] = posicion
+
+print(laminas_rep,laminas_en_memoria)
+
+
+
+
+#print(k , n)
---
+++ reto5.py
@@ -0,0 +1,113 @@
+import operator
+
+va_error = ""
+
+def promedio_precios(tienda_de_carlos):
+  promedio=0
+  for i in tienda_de_carlos:  
+    promedio+=tienda_de_carlos[i][1]
+  promedio = float ( promedio / len(tienda_de_carlos) )
+  return(round(promedio,1))
+
+def precio_mayor(tienda_de_carlos):  
+#   producto_mayor = []
+#   for i in tienda_de_carlos:          
+#     for key in tienda_de_carlos:  
+#         if (tienda_de_carlos[i][1] > tienda_de_carlos[key][1]):
+#             producto_mayor.append(tienda_de_carlos[i][0])
+#   producto_mayor.sort()  
+#   producto_mayor = producto_mayor[1]
+  producto_mayor = max(tienda_de_carlos[1].values())
+  return producto_mayor
+
+def total_inventario(tienda_de_carlos):  
+  total_invet  = float(0)
+  for i in tienda_de_carlos:          
+    total_invet = total_invet + (tienda_de_carlos[i][1] * tienda_de_carlos[i][2])
+  return total_invet
+  
+def precio_menor(tienda_de_carlos):  
+  producto_menor = ""
+  for i in tienda_de_carlos:          
+    for key in tienda_de_carlos:  
+        if (tienda_de_carlos[i][1] < tienda_de_carlos[key][1] and i != key):
+            producto_menor = tienda_de_carlos[i][0]  
+  return producto_menor
+
+def borrar_producto(tienda_de_carlos,producto):
+  if producto[0] in tienda_de_carlos:
+    tienda_de_carlos.pop(producto[0]) # ['Melon',70,13]
+    va_error = 'ok'
+   # print(tienda_de_carlos)
+  else:
+    va_error = 'ERROR'
+  return va_error
+
+
+def actualizar_producto(tienda_de_carlos,producto):
+  if producto[0] in tienda_de_carlos:
+    clave=producto[0] # [11,'Melon',70,13]
+    #del producto[0]
+    producto.pop(0) # ['Melon',70,13]
+    tienda_de_carlos[clave]= producto
+    va_error = 'ok'
+   # print(tienda_de_carlos)
+  else:
+    va_error = 'ERROR'
+  return va_error
+
+def agregar_producto(tienda_de_carlos,producto):
+  if producto[0] in tienda_de_carlos:
+    va_error = 'ERROR'
+  else:
+    clave=producto[0]
+    #del producto[0]
+    producto.pop(0) # ['Melon',70,13]
+    tienda_de_carlos[clave]= producto
+    va_error = 'ok'
+  #  print(tienda_de_carlos)
+  return va_error
+
+
+def leer_datos():
+  operacion= input() # AGREGAR ACTUALIZAR BORRAR
+  producto=input().split() # [11,'Melon',70,13] =producto
+  producto[0]= int(producto[0])
+  producto[2]= float(producto[2])
+  producto[3]= int(producto[3])
+  return operacion, producto
+
+def principal():
+  tienda_de_carlos={1 : ['Manzanas',8000.0,65],
+                  2 : ['Limones',2300.0,15],
+                  3 : ['Granadilla',2500.0,38],
+                  4 : ['Arandanos',9300.0,55],
+                  5 : ['Tomates',2100.0,42],
+                  6 : ['Fresas',4100.0,3],
+                  7 : ['Helado',4500.0,41],
+                  8 : ['Galletas',500.0,8],
+                  9 : ['Chocolates',3500.0,806],
+                 10 : ['Jamon',15000.0,10]}
+  
+  
+  operacion, producto=leer_datos()
+  validador = ''
+  if (operacion == 'AGREGAR' or operacion == 'agregar'):
+     validador = agregar_producto(tienda_de_carlos,producto)
+  elif (operacion == 'ACTUALIZAR' or operacion == 'actualizar' ):
+     validador = actualizar_producto(tienda_de_carlos,producto)
+  elif (operacion == 'BORRAR' or operacion == 'borrar'):
+     validador = borrar_producto(tienda_de_carlos,producto)
+  
+  if (validador == 'ok'):
+    precio_item_mayor = precio_mayor(tienda_de_carlos)
+    precio_item_menor = precio_menor(tienda_de_carlos)
+    promedito_inventario = promedio_precios(tienda_de_carlos)
+    total_inventario_t =  total_inventario(tienda_de_carlos)
+  #precio_mayor(tienda_de_carlos),precio_menor(tienda_de_carlos),promedio_precios(tienda_de_carlos)
+    print(precio_item_mayor,precio_item_menor,promedito_inventario,total_inventario_t)
+  else:
+      print('ERROR')
+
+
+principal()
